9c9eafda3396e87dbbce64b91e8ce7f7302578f4
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-06-28 16:41:55 -0400

    using code from contracsLab

--- app/server.js
+++ app/server.js
@@ -1,64 +1,38 @@
-const ethers = require('ethers');
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const server = express();
+import ethers from 'ethers'
+let ethers = require('ethers')
+require('dotenv').config()
+let utils = ethers.utils
+let SimpleBank = require('./dist/contracts/SimpleBank.json')
 
-//Get contract info 
-let contractInfo = require('../dist/contracts/Courestro.json');
-let bytecode = contractInfo.code;
-//let ABI = require('ABI.json');
-let ABI = contractInfo.abiDefinition;
+let simpleBankABI = SimpleBank.abiDefinition;
+let simpleBankBCODE = SimpleBank.code;
 
-let provider = ethers.getDefaultProvider('kovan');
+let provider = ethers.getDefaultProvider('ropsten')
 
+let privateKey = process.env.pk
 
-// view (function) || public variable 
-let readOnly = new ethers.Wallet(process.env.pk);
+let wallet = new ethers.Wallet(privateKey, provider)
 
-// can be used for anything 
-let readAndWrite = readOnly.connect(provider); //kovan
-let readAndWriteMainnet = readOnly.connect(mainnetProvider);
-let readAndWrite_2 = new ethers.Wallet(process.env.pk, provider);
+const contractAddress = '0x4931A34E108381ec659271a3A7FbbE6859c4019f'
+
+let readOnlyContract = new ethers.Contract(contractAddress, simpleBankABI, provider)
+let WRContact =  readOnlyContract.connect(wallet)
 
 
-let contractAddress = "0x692a70d2e424a56d2c6c27aa97d1a86395877b3a"
-let readOnlyContract = new ethers.Contract(contractAddress, ABI, provider); //contract -> kovan
+let  getBalance = async () =>  {
+  let balance = Number(await WRContact.balance()) // Because Map variable is Private cannot use READ-ONLY contract.
+  console.log(balance)
+}
 
-let readAndWriteContract = readOnlyContract.connect(readAndWrite);
+getBalance()
 
-// Call functions 
-async function getValue() {
-  let value = await readOnlyContract.getValue();
-  return value;
-} 
+let deposity = async () => {
+ let tx = await WRContact.deposit({value : utils.parseUnits('1', 'gwei')}) // returns tx
+ console.log(tx)
+}
 
-async function setValue(number) {
-  console.log(number);
-  let tx = await readAndWriteContract.setValue(number);
-  return tx;
-} 
+deposity()
 
-// Server
-server.set('port',process.env.PORT);
-server.use(bodyParser.json());
-
-// GET request, name: getValue
-server.get('/getValue', async (request, response) => {
-  let value = await getValue();
-  response.send(value.toNumber().toString());
-});
-
-server.post('/setValue', async (request, response) => {
-  let value = await setValue(request.body.integer);
-  response.send(value);
-});
-
-//Event Listener
-readAndWriteContract.on("ValueChanged", (author, oldVal, newVal) => {
-  console.log("author: ", author);
-  console.log("old, new ", oldVal.toNumber(), newVal.toNumber());
-});
-*/
-
-server.listen(process.env.PORT);
+WRContact.on("LogDepositMade", (sender, amount)=> {
+ console.log(`User : ${sender} has deposit ${amount}`);
+})
