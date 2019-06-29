2c225c8eb47ca33670848576a491447c3eea3278
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-06-29 01:26:46 -0400

    lab submitted

--- app/server.js
+++ app/server.js
@@ -1,9 +1,11 @@
-import ethers from 'ethers'
 let ethers = require('ethers')
-require('dotenv').config()
+require('dotenv').config({path: '../.env'})
 let utils = ethers.utils
-let SimpleBank = require('./dist/contracts/SimpleBank.json')
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
 
+let SimpleBank = require('../dist/contracts/SimpleBank.json')
 let simpleBankABI = SimpleBank.abiDefinition;
 let simpleBankBCODE = SimpleBank.code;
 
@@ -18,21 +20,43 @@ const contractAddress = '0x4931A34E108381ec659271a3A7FbbE6859c4019f'
 let readOnlyContract = new ethers.Contract(contractAddress, simpleBankABI, provider)
 let WRContact =  readOnlyContract.connect(wallet)
 
+server.use(bodyParser.json())
 
 let  getBalance = async () =>  {
   let balance = Number(await WRContact.balance()) // Because Map variable is Private cannot use READ-ONLY contract.
-  console.log(balance)
+  return balance
 }
 
-getBalance()
+let deposity = async (wei) => {
+  let tx = await WRContact.deposit({value : utils.parseUnits(parseInt(wei).toString(), 'gwei')}) // returns tx
+  return tx
+ }
 
-let deposity = async () => {
- let tx = await WRContact.deposit({value : utils.parseUnits('1', 'gwei')}) // returns tx
- console.log(tx)
-}
+server.get('/', async (req,res,next)=> {
+  res.send("Hello World")
+})
 
-deposity()
+server.get('/balance', async (req,res,next)=> {
+  let balance = await getBalance();
+  res.send(Number(balance).toString())
+})
+
+server.post('/deposite', async (req,res,next) => {
+  let response = await deposity(req.body.wei)
+  res.send(response)
+})
+
+
+
+
+// getBalance()
+
+
+
+// deposity('200000')
 
 WRContact.on("LogDepositMade", (sender, amount)=> {
  console.log(`User : ${sender} has deposit ${amount}`);
 })
+
+server.listen(3000)
